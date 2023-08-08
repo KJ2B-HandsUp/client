4375ef943b2f7fcd964e3b4dbe4ad307106ac750
min49590 <min49590@gmail.com>
2023-08-09 03:01:23 +0900

    Chore: 로그인 서버 연결 && 유저 정보 콘솔 출력

--- src/pages/RankingPage.tsx
+++ src/pages/RankingPage.tsx
@@ -2,8 +2,15 @@ import { RankingPageWrapper } from "../styled/ranking.styled";
 import { TopLeftButton } from "../styled/home.styled";
 import { AiOutlineHome } from "react-icons/ai";
 import { playBtnAudio } from "../utils/audio";
+import { useEffect } from "react";
 
 export default function RankingPage() {
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_LOGINSERVER_IP}/scan`)
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
       <RankingPageWrapper>
