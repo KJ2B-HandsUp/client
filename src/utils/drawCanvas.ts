9b9e537aad109aa274bef39b8fb815d4f6192872
min49590 <min49590@gmail.com>
2023-08-12 12:42:57 +0900

    Fixup: trycatch 추가

--- src/utils/drawCanvas.ts
+++ src/utils/drawCanvas.ts
@@ -17,25 +17,32 @@ export function drawCanvas(
   ctx.translate(-width, 0);
 
   ctx.drawImage(results.image, 0, 0, width, height);
-
-  if (results.multiHandLandmarks) {
-    let idx = 0;
-    for (const landmarks of results.multiHandLandmarks) {
-      const indexFingertip = landmarks[8];
-      if ("x" in indexFingertip && "y" in indexFingertip) {
-        ctx.beginPath();
-        ctx.arc(
-          indexFingertip.x * width,
-          indexFingertip.y * height,
-          fingerSize[idx],
-          0,
-          2 * Math.PI,
-        );
-        ctx.fillStyle = "#ffffff";
-        ctx.fill();
+  try {
+    if (results.multiHandLandmarks) {
+      let idx = 0;
+      for (const landmarks of results.multiHandLandmarks) {
+        const indexFingertip = landmarks[8];
+        if (
+          indexFingertip !== undefined &&
+          "x" in indexFingertip &&
+          "y" in indexFingertip
+        ) {
+          ctx.beginPath();
+          ctx.arc(
+            indexFingertip.x * width,
+            indexFingertip.y * height,
+            fingerSize[idx],
+            0,
+            2 * Math.PI,
+          );
+          ctx.fillStyle = "#ffffff";
+          ctx.fill();
+        }
+        idx++;
       }
-      idx++;
     }
+    ctx.restore();
+  } catch (err) {
+    console.log(err);
   }
-  ctx.restore();
 }
